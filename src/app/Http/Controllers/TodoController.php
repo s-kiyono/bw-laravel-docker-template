07cc47457628a2112eccf978507bd1160eb856a6
s-kiyono <iko.ruseinnto@gmail.com>
2025-12-01 08:34:29 +0900

    add:show.blade.phpの追加

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,15 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Todo;
+use Illuminate\Http\Request; /* use Illuminate\Http\Request; as Request とおなじ   */
+use App\Todo; /* use App\Todo as Todo とおなじ   */
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todo = new Todo();
+        $todo = new Todo(); /* new \App\Todo(); とおなじ */
         $todos = $todo->all();
+
         return view('todo.index',['todos' => $todos]);
     }
 
@@ -22,12 +23,21 @@ class TodoController extends Controller
     public function store(Request $request)
     {
         $inputs = $request->all();
-
+        dd($inputs);
+        
         $todo = new Todo();
         $todo->fill($inputs);
         $todo->save();
 
         return redirect()->route('todo.index');
+    }
+
+    public function show($id)
+    {
+        $model = new Todo();
+        $todo = $model->find($id);
+
+        return view('todo.show', ['todo' => $todo]);
     }
 
 }
