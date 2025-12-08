6c3a36aa8f8f3b03ecaa94a56bcd2ece70c96c4d
s-kiyono <iko.ruseinnto@gmail.com>
2025-12-09 08:35:45 +0900

    feat:Todo削除機能の実装

--- src/routes/web.php
+++ src/routes/web.php
@@ -22,3 +22,5 @@ Route::post('/todo', 'TodoController@store')->name('todo.store');
 Route::get('/todo/{id}', 'TodoController@show')->name('todo.show');
 Route::get('/todo/{id}/edit', 'TodoController@edit')->name('todo.edit');
 Route::put('/todo/{id}', 'TodoController@update')->name('todo.update');
+
+Route::delete('/todo/{id}/ydelete', 'TodoController@delete')->name('todo.delete');
